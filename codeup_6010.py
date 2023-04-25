176969e0ad2679ad886d31a28c3e9dc2bca4311b
blossom22 <ssunyaang@naver.com>
2023-04-25 21:19:10 +0900

    Initial commit

---
+++ codeup_6010.py
@@ -0,0 +1,7 @@
+# Codeup_Python 기초100제_6010: 정수 1개 입력받아 int로 변환하여 출력하기
+a = int(input("첫번째 수를 입력하세요: "))
+print(a, type(a))
+
+# int를 input으로 씌우지 않으면, 입력한 수의 type이 'str' class로 뜬다. 
+b = input("두번째 수를 입력하세요: ")
+print(b, type(b))
