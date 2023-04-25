df151cfd4e0d5e2a49b1847e9194b824d664d5a8
blossom22 <ssunyaang@naver.com>
2023-04-25 21:38:01 +0900

    Initial commit

---
+++ codeup_6011.py
@@ -0,0 +1,3 @@
+# Codeup_Python 기초100제_6011: 실수 1개 입력받아 변환하여 출력하기
+a = float(input("실수를 입력하세요: "))
+print(a, type(a))
---
+++ codeup_6012.py
@@ -0,0 +1,5 @@
+# Codeup_Python 기초100제_6012: 정수 2개 입력받아 그대로 출력하기
+a = int(input("첫번째 수를 입력하세요: "))
+b = int(input("두번째 수를 입력하세요: "))
+print(a)
+print(b)
