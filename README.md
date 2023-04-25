1634d1903658a825cd0808777309a386dc326701
blossom22 <ssunyaang@naver.com>
2023-04-25 15:55:44 +0900

    Description added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # codeup-practice
+# 이 파일들은 codeup의 Python 기초 100제를 다룬 것이다. (6001 ~ 6098번)
---
+++ codeup_6007.py
@@ -0,0 +1,2 @@
+# Codeup_Python 기초100제_6007: 윈도우 운영체제의 파일 경로를 출력(특수문자)하기
+print('"C:\\Download\\\'hello\'.py"')
