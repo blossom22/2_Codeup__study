6236c60f2430dbd0dbe07cc29a9b72d169eb9bb5
blossom22 <ssunyaang@naver.com>
2023-05-14 19:52:47 +0900

    Initial commit

---
+++ codeup_6035.py
@@ -0,0 +1,3 @@
+# Codeup_Python 기초100제_6035: 실수 2개 입력받아 곱 계산하기
+a, b = map(float, input("실수 2개를 입력하세요: ").split())
+print(a * b)
---
+++ codeup_6036.py
@@ -0,0 +1,5 @@
+# Codeup_Python 기초100제_6036: 단어 여러 번 출력하기
+
+a, b = input("반복할 단어와 반복횟수를 입력하세요: ").split()
+print(a * int(b))
+
