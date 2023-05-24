c7b6f12d2bab58a31976b977fb41973de705217b
blossom22 <ssunyaang@naver.com>
2023-05-24 17:08:14 +0900

    Initial commit

---
+++ codeup_6068.py
@@ -0,0 +1,10 @@
+# Codeup_Python 기초100제_6068: 점수 입력받아 평가 출력하기
+a = int(input("점수를 입력하세요: "))
+if a >= 90:
+    print("A")
+elif a>=70:        
+    print("B")
+elif a>=40:
+    print("C")
+else:
+    print("D")
