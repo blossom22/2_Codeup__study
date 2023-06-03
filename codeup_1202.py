c37d3c544a0380f043d893a3ee15eabef9cde448
blossom22 <ssunyaang@naver.com>
2023-06-03 16:36:39 +0900

    Initial commit

---
+++ codeup_1202.py
@@ -0,0 +1,13 @@
+# Codeup_Python 기초3.if~else_1202: 등급 판정
+
+a = int(input("정수를 입력하세요: "))
+if a>=90:
+    print("A")
+elif a>=80:
+    print("B")
+elif a>=70:
+    print("C")
+elif a>=60:
+    print("D")
+else:
+    print("F")
