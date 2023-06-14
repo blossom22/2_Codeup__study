1050df96cb19bde777daa03ef1e0b9bc783b419a
blossom22 <ssunyaang@naver.com>
2023-06-14 13:50:27 +0900

    Add another solution

--- codeup_1402.py
+++ codeup_1402.py
@@ -13,3 +13,6 @@ n = int(input("수를 입력하세요: "))
 a = list(map(int, input("데이터를 입력하세요: ").split()))
 for i in range(n-1,-1,-1):
     print(a[i],end=' ')
+
+# 코드를 최대한 줄이면 이렇게도 표현가능하다. 파이썬에서 ';'는 한줄에 여러문장을 쓸 때 사용되지만, 가독성을 위해 문장을 개행하여 구분하는 것이 권장된다. 
+n=int(input());a=list(map(int,input().split()));print(*a[::-1])
