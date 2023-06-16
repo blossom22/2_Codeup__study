0428472c228ed42ab393d390e7829b87fe865b5f
blossom22 <ssunyaang@naver.com>
2023-06-16 15:11:58 +0900

    Initial commit

--- codeup_1440.py
+++ codeup_1440.py
@@ -8,13 +8,13 @@ for i in range(n):
     b.append([])
 
 for i in range(n):
-    c = a[:]  # 원본 리스트를 복사하여 새로운 리스트 c를 생성
+    c = a[:]
     for j in range(n-1):
         d = c[i]
         del c[i]
         if a[i] > c[j]:
             b[i].append('>')
-            c.insert(i, d)  # 삭제한 값을 다시 삽입 (insert는   리스트.insert(들어갈index, 값)   꼴로 쓰인다)
+            c.insert(i, d)
         elif a[i] == c[j]:
             b[i].append('=')
             c.insert(i, d)
