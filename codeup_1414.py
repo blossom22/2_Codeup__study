fc82ce71f8745a8fa114d3c934deefd1cb93a778
blossom22 <ssunyaang@naver.com>
2023-06-17 15:36:31 +0900

    Add another solutiion

--- codeup_1414.py
+++ codeup_1414.py
@@ -7,3 +7,14 @@ e = ['cc', 'CC', 'cC', 'Cc']
 f = sum(1 for i in range(b - 1) if a[i:i+2] in e)
 print(d)
 print(f)
+
+
+# 아래는 d를 구할때, count를 쓰지 않고 구한 방식이다
+a = input()
+b = len(a)
+d = sum(1 for i in a if i == 'c' or i == 'C')
+e = {'cc', 'CC', 'cC', 'Cc'}
+f = sum(1 for i in range(b - 1) if a[i:i+2] in e)
+
+print(d)
+print(f)
