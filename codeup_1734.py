61f0616118f818c4c2fc3b85e0e55c91148896b8
blossom22 <ssunyaang@naver.com>
2023-06-17 17:19:31 +0900

    Initial commit

---
+++ codeup_1734.py
@@ -0,0 +1,10 @@
+# Codeup_Python 기초5-2.문자열_1734: welcome!
+
+# format 이용하는 방법
+print("welcome! {0}".format(input()))
+
+# f-string 이용하는 방법
+print(f"welcome! {input()}")
+
+# %기호 이용하는 방법
+print("welcome! %s"%input())
