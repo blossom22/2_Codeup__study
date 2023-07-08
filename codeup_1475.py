dd521fd0f3d7d15a52d4c1fdff880182ab60ea5c
blossom22 <ssunyaang@naver.com>
2023-07-08 09:27:39 +0900

    Initial commit

---
+++ codeup_1475.py
@@ -0,0 +1,17 @@
+# Codeup_Python 기초5-3.2차원배열_1475: 2차원 배열 지그재그 채우기 2-8
+
+a, b = map(int, input().split())
+arr = [[0] * b for _ in range(a)]
+c = 1
+
+for i in range(b-1,-1,-1):
+    if i % 2 == (b%2==0):
+        for j in range(a):
+            arr[j][i] = c 
+            c+=1 
+    else:
+        for j in range(a-1,-1,-1):
+            arr[j][i] = c 
+            c+=1 
+for i in arr:
+    print(*i)
