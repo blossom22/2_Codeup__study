c8f0d4c0b3bb806c716ce30f90a9483017a4ba5e
blossom22 <ssunyaang@naver.com>
2023-07-12 17:19:50 +0900

    Initial commit

---
+++ codeup_1485.py
@@ -0,0 +1,21 @@
+# Codeup_Python 기초5-3.2차원배열_1485: 2차원 배열 달팽이 채우기 4-2
+
+n,m = map(int, input().split())
+arr = [[0]*m for _ in range(n)]
+direct = [(0,1),(1,0),(0,-1),(-1,0)]
+direct_idx = 0
+row = 0
+col = 0
+
+for num in range(n*m,0,-1):
+    arr[row][col] = num
+
+    next_row = row + direct[direct_idx][0]
+    next_col = col + direct[direct_idx][1]
+
+    if next_row <0 or next_col <0 or next_row>=n or next_col>=m or arr[next_row][next_col] !=0 :
+        direct_idx = (direct_idx+1)%4
+    row += direct[direct_idx][0]
+    col += direct[direct_idx][1]
+for i in arr:
+    print(*i)
