5f9568643b4b2faaceb9a6596f5924e74d5e2730
blossom22 <ssunyaang@naver.com>
2023-07-14 16:26:37 +0900

    Add another solution

--- codeup_1482.py
+++ codeup_1482.py
@@ -14,3 +14,19 @@ for i in range(n+m-1, -1,-1):
 
 for i in matrix:
     print(*i)
+
+# 1478번 문제랑 대각선 그리는 방향이 같다. 단, a를 n*m에서 부터 빼주면 된다. 
+n,m = map(int, input().split())
+matrix = [[0]*m for _ in range(n)]
+
+a = n*m
+for i in range(n+m-1):
+    start_col = max(0, i-(m-1))
+    end_col = min(i, n-1)
+    for j in range(start_col, end_col+1):
+        k = i - j
+        matrix[j][m-1-k] = a
+        a -= 1
+
+for i in matrix:
+    print(*i)
