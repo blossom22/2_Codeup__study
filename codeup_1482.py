70ead58f9430d4fecc592bf5a76bfd7a85cec08a
blossom22 <ssunyaang@naver.com>
2023-07-14 16:13:43 +0900

    Initial commit

---
+++ codeup_1482.py
@@ -0,0 +1,16 @@
+# Codeup_Python 기초5-3.2차원배열_1482: 2차원 배열 빗금 채우기 3-7
+
+n,m = map(int, input().split())
+matrix = [[0]*m for _ in range(n)]
+
+a = n*m
+for i in range(n+m-1, -1,-1):
+    start = min(i, n-1)
+    end = max(0,i-(m-1))
+    for j in range(start, end-1, -1):   # 대각선을 채울때, 아래있는 것 먼저 채워서 for문을 역순으로 함
+        k = i - j
+        matrix[n-1-j][k] = a
+        a -= 1
+
+for i in matrix:
+    print(*i)
