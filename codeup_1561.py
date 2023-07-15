e00658f62b6cb3531b62d79e1538ca15144f440f
blossom22 <ssunyaang@naver.com>
2023-07-15 21:09:42 +0900

    Add another solution

--- codeup_1561.py
+++ codeup_1561.py
@@ -5,3 +5,10 @@ def f(n):
     print(n) if n>m else print(m)
 n,m = map(int, sys.stdin.readline().split())
 f(n)
+
+# max함수로 해결한 풀이
+import sys
+def f(n):
+    print(max(n,m))
+n,m = map(int, sys.stdin.readline().split())
+f(n)
