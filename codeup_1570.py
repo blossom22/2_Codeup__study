dc6a4678713d2b36ebe22b041a70d321deae9358
blossom22 <ssunyaang@naver.com>
2023-07-16 15:33:02 +0900

    Initial commit

---
+++ codeup_1570.py
@@ -0,0 +1,18 @@
+# Codeup_Python 기초100제_1570: 함수로 Lower Bound 위치 리턴하기
+
+import sys
+
+def f(a):
+    w = 0
+    while w < len(k):
+        if k[w] >= a:
+            print(w + 1)
+            break
+        else:
+            w += 1
+    else:
+        print(n+1)
+n = int(input())
+k = list(map(int, sys.stdin.readline().split()))
+a = int(input())
+f(a)
