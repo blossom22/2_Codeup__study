cc4dd0087882f59cba5d217338893eb9cbae75f8
blossom22 <ssunyaang@naver.com>
2023-07-16 15:16:16 +0900

    Initial commit

---
+++ codeup_1569.py
@@ -0,0 +1,9 @@
+# Codeup_Python 기초100제_1569: 함수로 배열의 최대값 위치 리턴하기 2
+
+import sys
+def f(a):
+    print(k.index(a)+1) if a in k else print(-1)
+n = int(input())
+k = list(map(int, sys.stdin.readline().split()))
+a = int(input())
+f(a)
